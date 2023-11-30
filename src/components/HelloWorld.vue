650b99ee05a3ff733d4b5eafad618c2addb7c5dd
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-11-30 18:24:58 -0300

    update methods

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,12 +10,22 @@ const sceneSizes = {
   height: 500,
 };
 
-const bancoInter = "/images/bancoInter.png";
-
-function createScene(canvas: HTMLDivElement) {
+const startScene = () => {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0xe2e2e2);
+  return scene;
+};
 
+const startRenderer = () => {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(sceneSizes.width, sceneSizes.height);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.setClearColor(0xa3a3a3);
+  return renderer;
+};
+
+const startCamera = () => {
   const camera = new THREE.PerspectiveCamera(
     60,
     sceneSizes.width / sceneSizes.height,
@@ -24,20 +34,13 @@ function createScene(canvas: HTMLDivElement) {
   );
 
   camera.position.x = 0;
-  camera.position.y = .5;
+  camera.position.y = 0.5;
   camera.position.z = 6.5;
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(sceneSizes.width, sceneSizes.height);
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-  renderer.setClearColor(0xa3a3a3);
+  return camera;
+};
 
-  canvas.appendChild(renderer.domElement);
-
-  const material = new THREE.MeshStandardMaterial({ color: 0x6e6e6e });
-  const controls = new OrbitControls(camera, renderer.domElement);
-
+const startDirectionalLight = (scene: THREE.Scene) => {
   const light = new THREE.DirectionalLight(0xffffff, 4);
   light.position.set(0, 5, 0.2);
   light.castShadow = true;
@@ -55,6 +58,10 @@ function createScene(canvas: HTMLDivElement) {
 
   const ambientLight = new THREE.AmbientLight(0xfffeee, 5);
   scene.add(ambientLight);
+};
+
+const addPodiumBase = (scene: THREE.Scene) => {
+  const material = new THREE.MeshStandardMaterial({ color: 0x6e6e6e });
 
   const firstPlaceGeometry = new THREE.BoxGeometry(5, 1, 2);
   const firstPlaceObject = new THREE.Mesh(firstPlaceGeometry, material);
@@ -65,40 +72,86 @@ function createScene(canvas: HTMLDivElement) {
   const secondPlaceGeometry = new THREE.BoxGeometry(4, 0.6, 2);
   const secondPlaceObject = new THREE.Mesh(secondPlaceGeometry, material);
   secondPlaceObject.position.set(4, -2.2, 0);
+  secondPlaceObject.receiveShadow = true;
   scene.add(secondPlaceObject);
 
   const thirdPlaceObject = new THREE.Mesh(secondPlaceGeometry, material);
   thirdPlaceObject.position.set(-4, -2.2, 0);
+  thirdPlaceObject.receiveShadow = true;
   scene.add(thirdPlaceObject);
+};
 
+const createPenny = (imageUrl: string, radius: number) => {
   const pennyMaterials = [];
-  const pennyGeometry = new THREE.CylinderGeometry(1.5, 1.5, 0.1, 32);
+  const pennyGeometry = new THREE.CylinderGeometry(radius, radius, 0.1, 32);
   const pennySideMaterial = new THREE.MeshLambertMaterial({ color: 0x4f3e37 });
 
   pennyMaterials.push(pennySideMaterial);
 
   const headsLoader = new THREE.TextureLoader();
-  headsLoader.load(bancoInter, (texture: THREE.Texture) => {
+  headsLoader.load(imageUrl, (texture: THREE.Texture) => {
     const materialTop = new THREE.MeshBasicMaterial({ map: texture });
     pennyMaterials.push(materialTop);
   });
 
   const tailsLoader = new THREE.TextureLoader();
-  tailsLoader.load(bancoInter, (texture: THREE.Texture) => {
+  tailsLoader.load(imageUrl, (texture: THREE.Texture) => {
     texture.wrapS = THREE.RepeatWrapping;
-    texture.repeat.x = - 1;
+    texture.repeat.x = -1;
     texture.flipY = false;
     const materialBottom = new THREE.MeshBasicMaterial({ map: texture });
     pennyMaterials.push(materialBottom);
   });
 
-  const firstPenny = new THREE.Mesh(pennyGeometry, pennyMaterials);
+  return new THREE.Mesh(pennyGeometry, pennyMaterials);
+}
+
+function startCanvas(canvas: HTMLDivElement) {
+  // instancia a cena
+  const scene = startScene();
+
+  // instancia a camera
+  const camera = startCamera();
+
+  // instancia o renderer
+  const renderer = startRenderer();
+
+  // adiciona o canvas
+  canvas.appendChild(renderer.domElement);
+
+  // habilita os controles
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // instancia a luz
+  startDirectionalLight(scene);
+
+  // adiciona o pódio na cena
+  addPodiumBase(scene);
+
+  // adiciona a medalha
+  const firstPenny = createPenny("/images/bancoInter.png", 1.5);
 
   firstPenny.position.set(0, 0.7, 0.3);
   firstPenny.rotateX(1.56);
   firstPenny.rotateY(1.56);
   firstPenny.castShadow = true;
   scene.add(firstPenny);
+
+  const secondPenny = createPenny("/images/bancoItau.png", 1.2);
+  secondPenny.position.set(4, -0.5, 0.3);
+  secondPenny.rotateX(1.56);
+  secondPenny.rotateY(1.56);
+  secondPenny.castShadow = true;
+  scene.add(secondPenny);
+
+  const thirdPenny = createPenny("/images/bancoSantander.png", 1.2);
+  thirdPenny.position.set(-4, -0.5, 0.3);
+  thirdPenny.rotateX(1.56);
+  thirdPenny.rotateY(1.56);
+  thirdPenny.castShadow = true;
+  scene.add(thirdPenny);
+
+  // anima a medalha
 
   const firstPennyStart = new TWEEN.Tween({
     y: 0,
@@ -122,7 +175,10 @@ function createScene(canvas: HTMLDivElement) {
   firstPennyEnd.chain(firstPennyStart);
   firstPennyStart.start();
 
+  // inicia a animação
   function animate() {
+    secondPenny.rotateX(0.01);
+    thirdPenny.rotateX(-0.01);
     TWEEN.update();
     controls.update();
     renderer.render(scene, camera);
@@ -132,7 +188,7 @@ function createScene(canvas: HTMLDivElement) {
 }
 
 onMounted(() => {
-  if (bestMatchScene.value) createScene(bestMatchScene.value);
+  if (bestMatchScene.value) startCanvas(bestMatchScene.value);
 });
 </script>
 
